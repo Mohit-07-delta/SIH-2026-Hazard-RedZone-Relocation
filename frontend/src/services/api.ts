30e2d1bd2c90fe4cdda9b238edbb82313608b56b
Satyam-Viz <satyam963670@gmail.com>
2026-09-05 10:48:43 +0000

    removed

--- frontend/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'http://localhost:3001/api';
-
-export const fetchHazards = () => axios.get(`${API_BASE}/hazards`).then(res => res.data);
-export const fetchHabitations = () => axios.get(`${API_BASE}/habitations`).then(res => res.data);
-export const fetchSafeZones = () => axios.get(`${API_BASE}/safezones`).then(res => res.data);
-export const fetchRelocationPlan = () => axios.get(`${API_BASE}/relocation-plan`).then(res => res.data);
